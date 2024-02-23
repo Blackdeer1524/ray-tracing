3722d8831ea5f532d47cb2311cd1a37f17eeb2dc
Danila Slesarev <blackdeer1524@yandex.ru>
2024-02-23 16:41:14 +0300

    fuzz

--- main.cpp
+++ main.cpp
@@ -23,8 +23,8 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(
         make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
@@ -35,7 +35,7 @@ int main() {
 
     camera cam{};
 
-    cam.image_width = 400;
+    cam.image_width = 300;
     cam.aspect_ratio = 16.0 / 9.0;
     cam.samples_per_pixel = 10;
     cam.render(world);
