88b4c3227434f7d2d9af49cf8d6b3f3dac016661
Danila Slesarev <blackdeer1524@yandex.ru>
2024-02-23 11:50:23 +0300

    camera in a separate class

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "camera.h"
 #include "color.h"
 #include "hittable.h"
 #include "lib/hittable_list/hittable_list.h"
@@ -20,48 +21,14 @@ color ray_color(const ray &r, const hittable &world) {
 
 int main() {
     // Image
-    const double aspect_ratio = 16.0 / 9.0;
-    int image_width = 1920;
-    int image_height = static_cast<int>(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
-    // World
-
+    // const double aspect_ratio = 16.0 / 9.0;
     hittable_list world;
 
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+    camera cam{};
 
-    const double focal_length = 1.0;
-    const double viewport_height = 2.0;
-    const auto viewport_width =
-        viewport_height * (static_cast<double>(image_width) / image_height);
-
-    const auto v_u = vec3(viewport_width, 0, 0);
-    const auto v_v = vec3(0, -viewport_height, 0);
-
-    const auto delta_u = viewport_width / image_width;
-    const auto delta_v = -viewport_height / image_height;
-
-    const point3 camera_center(0, 0, 0);
-    point3 viewport_position = camera_center + vec3(0, 0, -focal_length);
-
-    point3 p00_position = viewport_position - v_u / 2 - v_v / 2 +
-                          vec3(delta_u / 2, 0, 0) + vec3(0, delta_v / 2, 0);
-
-    // Render
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-    for (int i = 0; i < image_height; ++i) {
-        std::clog << "\rScanlines remaining: " << (image_height - i) << ' '
-                  << std::flush;
-        for (int j = 0; j < image_width; ++j) {
-            const auto shooting_pos =
-                p00_position + vec3(delta_u * j, delta_v * i, 0);
-            const ray r(camera_center, shooting_pos - camera_center);
-            const auto c = ray_color(r, world);
-
-            write_color(std::cout, c);
-        }
-    }
-    std::clog << "\rDone.                 \n";
+    cam.image_width = 1920;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.render(world);
 }
