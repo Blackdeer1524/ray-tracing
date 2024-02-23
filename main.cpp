56b402be39fc568af0e646327aa1e48052778e75
Danila Slesarev <blackdeer1524@yandex.ru>
2024-02-23 18:20:49 +0300

    refraction

--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ int main() {
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(
         make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
@@ -35,7 +35,7 @@ int main() {
 
     camera cam{};
 
-    cam.image_width = 300;
+    cam.image_width = 400;
     cam.aspect_ratio = 16.0 / 9.0;
     cam.samples_per_pixel = 10;
     cam.render(world);
