1a1770924ffd5e01b1e702d361f1dda9a44676d5
Danila Slesarev <blackdeer1524@yandex.ru>
2024-02-29 06:38:43 +0300

    todos

--- lib/camera/camera.h
+++ lib/camera/camera.h
@@ -8,23 +8,22 @@
 #include "material.h"
 #include "vec3.h"
 #include <cmath>
+#include <iostream>
 #include <thread>
 #include <vector>
-#include <iostream>
 
 class camera {
  public:
     float aspect_ratio = 1.0;
     int image_width = 100;
     int samples_per_pixel = 10;
-    int max_depth = 10;  // Maximum number of ray bounces into scene
-    float vfov = 90;     // in degrees
+    int max_depth = 10;
+    float vfov = 90;
 
-    point3 lookfrom = point3(0, 0, 0);  // Point camera is looking from
-    point3 lookat = point3(0, 0, -1);   // Point camera is looking at
-    vec3 vup = vec3(0, 1, 0);           // Camera-relative "up" direction
+    point3 lookfrom = point3(0, 0, 0);
+    point3 lookat = point3(0, 0, -1);
+    vec3 vup = vec3(0, 1, 0);  // Camera-relative "up" direction
 
-    float defocus_angle = 0;  // Variation angle of rays through each pixel
     float focus_dist =
         10;  // Distance from camera lookfrom point to plane of perfect focus
 
@@ -86,9 +85,6 @@ class camera {
 
     vec3 w, u, v;
 
-    vec3 defocus_disk_u;  // Defocus disk horizontal radius
-    vec3 defocus_disk_v;  // Defocus disk vertical radius
-
     void initialize() {
         image_height = static_cast<int>(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
@@ -114,24 +110,13 @@ class camera {
 
         p00_position =
             viewport_center - v_u / 2 - v_v / 2 + delta_u / 2 + delta_v / 2;
-
-        // Calculate the camera defocus disk basis vectors.
-        auto defocus_radius =
-            focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
-        defocus_disk_u = u * defocus_radius;
-        defocus_disk_v = v * defocus_radius;
     }
 
     ray get_ray(int i, int j) {
-        // Get a randomly-sampled camera ray for the pixel at location i,j,
-        // originating from the camera defocus disk.
-
         const auto shooting_pos = p00_position + delta_u * j + delta_v * i;
         const auto sampled_shooting_pos = shooting_pos + pixel_sample_suquare();
 
-        auto ray_origin =
-            (defocus_angle <= 0) ? camera_center : defocus_disk_sample();
-
+        auto ray_origin = camera_center;
         return ray(camera_center, sampled_shooting_pos - camera_center);
     }
 
@@ -139,14 +124,8 @@ class camera {
         return delta_u / 2 * random_float() + delta_v / 2 * random_float();
     }
 
-    point3 defocus_disk_sample() const {
-        // Returns a random point in the camera defocus disk.
-        auto p = random_in_unit_disk();
-        return camera_center + (p[0] * defocus_disk_u) +
-               (p[1] * defocus_disk_v);
-    }
-
-    color ray_color(ray r, const int max_depth, const hittable &world) const {
+    [[nodiscard]] color
+    ray_color(ray r, const int max_depth, const hittable &world) const {
         color cumulative_attenuation(1.0, 1.0, 1.0);
         int i = 0;
         for (; i < max_depth; ++i) {
--- main.cpp
+++ main.cpp
@@ -59,14 +59,13 @@ int main() {
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 1200;
     cam.samples_per_pixel = 100;
-    cam.max_depth = 10;
+    cam.max_depth = 20;
 
     cam.vfov = 20;
     cam.lookfrom = point3(13, 2, 3);
     cam.lookat = point3(0, 0, 0);
     cam.vup = vec3(0, 1, 0);
 
-    cam.defocus_angle = 0.0;
     cam.focus_dist = 10.0;
 
     cam.render(world);
